5f68eb4c24d681970504d4d1fa124bc1d69f9638
Graeme Hill <graemekh@gmail.com>
2017-10-27 20:01:04 -0700

    Partial non-work file watcher

--- src/main/java/ca/graemehill/synctool/actors/WatchActor.java
+++ src/main/java/ca/graemehill/synctool/actors/WatchActor.java
@@ -3,6 +3,10 @@ package ca.graemehill.synctool.actors;
 import akka.actor.AbstractActor;
 import ca.graemehill.synctool.Log;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.*;
+
 public class WatchActor extends AbstractActor {
     private static Watcher watcher = new Watcher();
 
@@ -15,7 +19,11 @@ public class WatchActor extends AbstractActor {
     }
 
     private void watch(StartWatching msg) {
-        watcher.watch(msg.getPath());
+        try {
+            watcher.watch(msg.getPath());
+        } catch (Exception e) {
+            Log.error("Would not watch " + msg.getPath(), e);
+        }
     }
 
     private void stopWatching(StopWatching msg) {
@@ -23,8 +31,15 @@ public class WatchActor extends AbstractActor {
     }
 
     public static class Watcher {
-        public void watch(String path) {
+        public void watch(String path) throws IOException {
             Log.info("WATCH " + path);
+            WatchService watchService = FileSystems.getDefault().newWatchService();
+            Path pathToWatch = new File(path).toPath();
+            WatchKey key = pathToWatch.register(
+                watchService,
+                StandardWatchEventKinds.ENTRY_CREATE,
+                StandardWatchEventKinds.ENTRY_DELETE,
+                StandardWatchEventKinds.ENTRY_MODIFY);
         }
         public void stop(String path) {
             Log.info("STOP WATCHING " + path);
